7dfdf06369ebac5f8deaac6204a487fd57807412
Lucas Brisch <lucasbrisch@gmail.com>
2026-03-19 20:45:02 -0300

    Implement flood-fill stack

---
+++ FloodFill/src/FloodFillStack.java
@@ -0,0 +1,46 @@
+public class FloodFillStack {
+    private Node<Point> top;
+
+    public FloodFillStack() {
+        top = null;
+    }
+
+    public void push(Point p) {
+        Node<Point> newNode = new Node<>(p);
+        newNode.next = top;
+        top = newNode;
+    }
+
+    public Point pop() {
+        if (top == null) return null;
+        Point p = top.data;
+        top = top.next;
+        return p;
+    }
+
+    public boolean isEmpty() {
+        return top == null;
+    }
+
+    public void fill(int[][] grid, int startX, int startY) {
+        if (grid[startX][startY] != 0) return;
+
+        push(new Point(startX, startY));
+
+        while (!isEmpty()) {
+            Point p = pop();
+            int x = p.x;
+            int y = p.y;
+
+            if (x < 0 || y < 0 || x >= grid.length || y >= grid[0].length) continue;
+            if (grid[x][y] != 0) continue;
+
+            grid[x][y] = 2;
+
+            push(new Point(x + 1, y));
+            push(new Point(x - 1, y));
+            push(new Point(x, y + 1));
+            push(new Point(x, y - 1));
+        }
+    }
+}
--- FloodFill/src/Screen.java
+++ FloodFill/src/Screen.java
@@ -1,34 +1,44 @@
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.*;
 
 public class Screen extends JPanel {
     private int[][] pixels = new int[8][8];
+    private FloodFillStack FloodFillStack = new FloodFillStack();
 
     public Screen() {
-
         for (int i = 0; i < 8; i++) {
             pixels[i][7 - i] = 1;
         }
+
+        addMouseListener(new MouseAdapter() {
+            @Override
+            public void mouseClicked(MouseEvent e) {
+                int pixelSize = 50;
+                int x = e.getX() / pixelSize;
+                int y = e.getY() / pixelSize;
+                if (x >= 0 && x < 8 && y >= 0 && y < 8) {
+                    FloodFillStack.fill(pixels, x, y);
+                    repaint();
+                }
+            }
+        });
     }
 
     @Override
     protected void paintComponent(Graphics g) {
         super.paintComponent(g);
-
         int pixelSize = 50;
-
         for (int x = 0; x < 8; x++) {
             for (int y = 0; y < 8; y++) {
-
                 if (pixels[x][y] == 1) {
                     g.setColor(Color.BLACK);
+                } else if (pixels[x][y] == 2) {
+                    g.setColor(Color.BLUE);
                 } else {
                     g.setColor(Color.WHITE);
                 }
-
                 g.fillRect(x * pixelSize, y * pixelSize, pixelSize, pixelSize);
-
-                // grid border (for pixel look)
                 g.setColor(Color.GRAY);
                 g.drawRect(x * pixelSize, y * pixelSize, pixelSize, pixelSize);
             }
