5e665a470bc477ddad3f19e517bc294d1b254bfb
Lucas Brisch <lucasbrisch@gmail.com>
2026-03-26 19:23:06 -0300

    fixing yayy

--- FloodFill/src/FloodFillStack.java
+++ FloodFill/src/FloodFillStack.java
@@ -1,46 +1,32 @@
 public class FloodFillStack implements FillAlgorithm {
-    private Node<Point> top;
+    private Stack<Point> stack;
 
     public FloodFillStack() {
-        top = null;
-    }
-
-    public void push(Point p) {
-        Node<Point> newNode = new Node<>(p);
-        newNode.next = top;
-        top = newNode;
-    }
-
-    public Point pop() {
-        if (top == null) return null;
-        Point p = top.data;
-        top = top.next;
-        return p;
-    }
-
-    public boolean isEmpty() {
-        return top == null;
+        this.stack = new Stack<>();
     }
 
     public void fill(int[][] grid, int startX, int startY) {
         if (grid[startX][startY] != 0) return;
 
-        push(new Point(startX, startY));
+        stack.push(new Point(startX, startY));
 
-        while (!isEmpty()) {
-            Point p = pop();
+        while (!stack.isEmpty()) {
+            Point p = stack.pop();
+            if (p == null) continue;
+
             int x = p.x;
             int y = p.y;
 
             if (x < 0 || y < 0 || x >= grid.length || y >= grid[0].length) continue;
+
             if (grid[x][y] != 0) continue;
 
             grid[x][y] = 2;
 
-            push(new Point(x + 1, y));
-            push(new Point(x - 1, y));
-            push(new Point(x, y + 1));
-            push(new Point(x, y - 1));
+            stack.push(new Point(x + 1, y));
+            stack.push(new Point(x - 1, y));
+            stack.push(new Point(x, y + 1));
+            stack.push(new Point(x, y - 1));
         }
     }
 }
